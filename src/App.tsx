07f50c52458c946866d48395d5be7bd5367eedb1
Stanislav Moskalenko <maskal2719@gmail.com>
2023-03-02 15:27:18 +0300

    lesson 1-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavLink to={'/'}>Main</NavLink>---------
+            <NavLink to={'/login'}>Login</NavLink>-------
+            <NavLink to={'/profile'}>Profile</NavLink>-----
+            <NavLink to={'/profile/setting'}>Setting</NavLink>
+
+            <Routes>
+                <Route path={'/*'} element={<div>404</div>}/>
+                <Route path={'/'} element={<div>main</div>}/>
+                <Route path={'/login'} element={<div>login</div>}/>
+                <Route path={'/profile/*'} element={
+                    <div>
+                        profile
+                        <Routes>
+                            <Route path={'/setting'} element={<div>setting</div>}/>
+                        </Routes>
+                    </div>
+                }/>
+
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
